985412a6b6f566195663d6a5565f06df7f3b48ef
Dakota Crane <djc2702@uw.edu>
2017-08-25 14:59:30 -0700

    added modifiers (e.g. 2d6+1) to dice roller

--- modules/dice.py
+++ modules/dice.py
@@ -11,13 +11,24 @@ class dice():
 
     @commands.command(description="roll dice in NdN format.")
     async def roll(self, dice : str):
+        mod = -1
         try:
+            if '+' in dice:
+                mod = dice[dice.index('+') + 1:]
+                dice = dice[0:dice.index('+')]
             rolls, limit = map(int, dice.split('d'))
         except Exception:
-            await self.bot.say('Format has to be in NdN!')
+            await self.bot.say('Format has to be in NdN+x!')
             return
-
+        resultsum = 0
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        for r in (result.split(', ')):
+            resultsum += int(r)
+        if mod != -1:
+            result += ' = ' + str(resultsum)
+            result += ' + ' + str(mod)
+            resultsum += int(mod)
+        result += ' = ' + str(resultsum)
         result = prefix.choosePrefix() + result
         await self.bot.say(result)
 
