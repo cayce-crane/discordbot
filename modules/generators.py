b0872bc90f777401eacac48bfd0efe36ab1e744b
Dakota Crane <djc2702@uw.edu>
2017-09-04 21:12:22 -0700

    added ability to do multiple chars at once, and also do spells on their own

--- modules/generators.py
+++ modules/generators.py
@@ -13,14 +13,23 @@ class generators():
 
     @commands.command(description="Generate a variety of things. Syntax: ']random <schema> <argument>' \n " \
         "e.g. ']random mazerats character'")
-    async def random(self, schema : str, arg : str):
+    async def random(self, schema : str, arg : str, count=1):
      #   try:
+        i = 0
         if schema == 'mazerats':
+            ch = Character()
             if arg == 'character':
                 result = prefix.choosePrefix()
-                ch = Character()
-                result += ch.returnChar()
-                await self.bot.say(result)
+                while (i < count):
+                    ch = Character()
+                    result += ch.returnChar() + "\n\n"
+                    i += 1
+            if arg == 'spell':
+                result = prefix.choosePrefix()
+                while (i < count):
+                    result += ch.randomSpell() + "\n"
+                    i += 1
+        await self.bot.say(result)
      #   except Exception:
       #      await self.bot.say('Format must be "]random <schema> <argument>".')
       #      return
